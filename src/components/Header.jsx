1697fe91761224f7bcd94e3f9252871b0a0665d7
mknyambura <kariuki.mercie@gmail.com>
2023-06-01 22:16:15 +0300

    update README

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -7,10 +7,10 @@ function Header() {
     <div className='flex flex-row justify-between m-16'>
         <h1 className='font-bold text-4xl'>FinanceX</h1>
         <div className='text-white/70 flex flex-row justify-between gap-8'>
-            <h5>Why FinanceX</h5>
-            <h5>Features</h5>
-            <h5>Update</h5>
-            <h5>Blog</h5>
+            <h6>Why FinanceX</h6>
+            <h6>Features</h6>
+            <h6>Update</h6>
+            <h6>Blog</h6>
         </div>
         <button className='items-center px-4 py-2 border border-transparent bg-transparent flex flex-row gap-2 hover:bg-[#CBFC01] transition-all duration-500 hover:text-black font-bold rounded-md'>
             Sign Up
